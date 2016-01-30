0669254f802f607405e081ee4d53ebbe1f636e7c
Shane Grüling <shane.grueling@portalwelten.de>
2016-01-30 01:04:52 +0100

    Movement test

---
+++ Assets/MoveToClick.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Node = NavGrid.Node;
+
+public class MoveToClick : MonoBehaviour {
+    NavMeshAgent agent;
+    private Vector3 destination;
+    private NavGrid grid;
+    private Queue<Node> _path;
+
+    void Start()
+    {
+        grid = FindObjectOfType<NavGrid>();
+        _path = new Queue<Node>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var mousPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousPoint.z = 0;
+            FindPath(transform.position, mousPoint);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_path.Count == 0) return;
+
+        var target = _path.Peek();
+
+        if (target.WorldPosition.x > Math.Round(transform.position.x*2)/2)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            GetComponent<Animator>().SetInteger("Direction", 2);
+        }
+        else if (target.WorldPosition.x < Math.Round(transform.position.x * 2) / 2)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            GetComponent<Animator>().SetInteger("Direction", 4);
+        }
+        else if (target.WorldPosition.y > Math.Round(transform.position.y * 2) / 2)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            GetComponent<Animator>().SetInteger("Direction", 1);
+        }
+        else if (target.WorldPosition.y < Math.Round(transform.position.y * 2) / 2)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            GetComponent<Animator>().SetInteger("Direction", 3);
+        }
+
+        Debug.Log(GetComponent<Animator>().GetInteger("Direction"));
+
+        transform.position = Vector3.MoveTowards(transform.position, target.WorldPosition, 1f * Time.fixedDeltaTime);
+
+        
+
+        if (transform.position == target.WorldPosition)
+        {
+            _path.Dequeue();
+        }
+    }
+
+    public void LateUpdate()
+    {
+        var position = transform.position;
+
+        position.z = Camera.main.transform.position.z;
+
+        Camera.main.transform.position = position;
+    }
+
+    private class Waypoint
+    {
+        public Node Node;
+        public int GCost;
+        public int FCost { get { return GCost + HCost; } }
+        public int HCost;
+
+        public Waypoint Parent { get; internal set; }
+
+        public override int GetHashCode()
+        {
+            return Node.GetHashCode();
+        }
+    }
+
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Waypoint startNode = new Waypoint
+        {
+            Node = grid.NodeFromWorldPoint(startPos),
+            GCost = 0,
+        };
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        if (!targetNode.Walkable) return;
+
+        List<Waypoint> openSet = new List<Waypoint>();
+        HashSet<Waypoint> closedSet = new HashSet<Waypoint>();
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            Waypoint currentNode = openSet[0];
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].FCost < currentNode.FCost || openSet[i].FCost == currentNode.FCost && openSet[i].HCost <= currentNode.HCost)
+                {
+                    currentNode = openSet[i];
+                }
+            }
+
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if (currentNode.Node == targetNode)
+            {
+                RetracePath(startNode.Node, targetNode, currentNode);
+                return;
+            }
+
+            foreach (Node neighbour in grid.GetNeighbours(currentNode.Node))
+            {
+                var wayPoint = new Waypoint() { Node = neighbour };
+                if (!neighbour.Walkable || closedSet.Contains(wayPoint))
+                {
+                    continue;
+                }
+
+                int newMovementCostToNeighbour = currentNode.GCost + 1;
+                if (newMovementCostToNeighbour < wayPoint.GCost || !openSet.Contains(wayPoint))
+                {
+                    wayPoint.GCost = newMovementCostToNeighbour;
+                    wayPoint.HCost = (int)distance(neighbour.X, neighbour.Y, targetNode.X, targetNode.Y);
+                    wayPoint.Parent = currentNode;
+
+                    if (!openSet.Contains(wayPoint))
+                        openSet.Add(wayPoint);
+                }
+            }
+        }
+
+    }
+
+    private double distance(int x1, int y1, int x2, int y2)
+    {
+        int dx = Math.Abs(x2 - x1);
+        int dy = Math.Abs(y2 - y1);
+
+        int min = Math.Min(dx, dy);
+        int max = Math.Max(dx, dy);
+
+        int diagonalSteps = min;
+        int straightSteps = max - min;
+
+        return Math.Sqrt(2) * diagonalSteps + straightSteps;
+    }
+
+    void RetracePath(Node startNode, Node endNode, Waypoint endWayPoint)
+    {
+        List<Node> path = new List<Node>();
+        var currentNode = endWayPoint;
+
+        while (currentNode.Node != startNode)
+        {
+            path.Add(currentNode.Node);
+            currentNode = currentNode.Parent;
+        }
+        path.Reverse();
+
+        grid.path = path;
+
+        _path = new Queue<Node>(path);
+
+    }
+}
---
+++ Assets/NavGrid.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class NavGrid : MonoBehaviour {
+
+    public class Node
+    {
+        public bool Walkable;
+        public Vector3 WorldPosition;
+        public int X;
+        public int Y;
+    }
+
+    public Vector3 BottomLeft;
+
+    public int Width;
+    public int Height;
+
+    private Node[,] grid;
+
+	// Use this for initialization
+	void Start () {
+        grid = new Node[Width, Height];
+
+        for (int x = 0; x < Width; x++)
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                Vector3 worldPoint = BottomLeft + Vector3.right * (x) + Vector3.up * (y);
+                bool walkable = !(Physics2D.CircleCast(worldPoint, 0.1f, Vector3.zero));
+                
+                grid[x, y] = new Node
+                {
+                    Walkable = walkable,
+                    WorldPosition = worldPoint,
+                    X = x,
+                    Y = y,
+                };
+            }
+        }
+    }
+
+    public List<Node> GetNeighbours(Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0
+                    || x == -1 && y == -1
+                    || x == 1 && y == 1
+                    || x == 1 && y == -1
+                    || x == -1 && y == 1)
+                    continue;
+
+                int checkX = node.X + x;
+                int checkY = node.Y + y;
+
+                if (checkX >= 0 && checkX < Width && checkY >= 0 && checkY < Height)
+                {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
+    public Node NodeFromWorldPoint(Vector3 worldPosition)
+    {
+        int x = (int)Math.Round(worldPosition.x - BottomLeft.x);
+        int y = (int)Math.Round(worldPosition.y - BottomLeft.y);
+
+        return grid[x, y];
+    }
+
+    public List<Node> path;
+    void OnDrawGizmos()
+    {
+        var center = BottomLeft;
+
+        center.x = (center.x - 0.5f ) + Width / 2;
+        center.y = (center.y -0.5f) + Height / 2;
+        Gizmos.DrawWireCube(center, new Vector3(Width, Height, 1));
+
+        if (grid != null)
+        {
+            foreach (Node n in grid)
+            {
+                Gizmos.color = (n.Walkable) ? Color.white : Color.red;
+                if (path != null)
+                    if (path.Contains(n))
+                        Gizmos.color = Color.black;
+                Gizmos.DrawCube(n.WorldPosition, Vector3.one);
+            }
+        }
+    }
+}
